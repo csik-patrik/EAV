42cda51ca031570516765508c01d650e9a593b7e
csik-patrik <csik.patrik455@gmail.com>
2022-07-21 09:14:42 +0200

    Update index page table

--- resources/views/EntityType/index.blade.php
+++ resources/views/EntityType/index.blade.php
@@ -3,23 +3,32 @@
 @section('title') Entity Types @endsection
 
 @section('content')
-    <div class="container p-3">
-        <table class="table">
-            <thead class="thead-dark">
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Label</th>
-            </tr>
-            </thead>
-            <tbody>
-                @foreach ($entityTypes as $entityType)
+    <div class="container-fluid p-3">
+        <button type="button" class="btn btn-success">Add Entity Type</button>
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                      <th scope="col">Id</th>
+                      <th scope="col">Label</th>
+                      <th scope="col">Method</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach ($entityTypes as $entityType)
                     <tr>
                         <td>{{$entityType->id}}</td>
                         <td>{{$entityType->label}}</td>
+                        <td>
+                            <button type="button" class="btn btn-danger">Danger</button>
+                            <button type="button" class="btn btn-warning">Warning</button>
+                        </td>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                    @endforeach
+                    
+                  </tbody>
+            </table>
+          </div>
     </div>
     
 @endsection
