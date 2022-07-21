be5a569c51b273d2e23c67712d9bbe1fe6cc845f
csik-patrik <csik.patrik455@gmail.com>
2022-07-21 08:46:39 +0200

    Add layout blade

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>@yield('title')</title>
+        @yield('links')
+        @yield('scripts')
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
