9923cb9c4ea23b49b300ade928694da1f147c030
csik-patrik <csik.patrik455@gmail.com>
2022-07-21 19:30:05 +0200

    Change id field type to bigIncrements

--- database/migrations/2022_07_21_061542_create_entity_types_table.php
+++ database/migrations/2022_07_21_061542_create_entity_types_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('entity_types', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('label');
             $table->timestamps();
         });
