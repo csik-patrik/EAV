03a87379269ea918398581da2e76eabe5302095b
csik-patrik <csik.patrik455@gmail.com>
2022-07-21 08:21:13 +0200

    Implement EntityType model

---
+++ app/Models/EntityType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EntityType extends Model
+{
+    use HasFactory;
+
+    protected $table = 'entity_types';
+
+    protected $fillable = ['label'];
+
+}
