591ba1d0ea7177d46a72b9d15410675d68c9b324
csik-patrik <csik.patrik455@gmail.com>
2022-07-25 16:35:39 +0200

    Implement attribute index page

---
+++ resources/views/Attribute/index.blade.php
@@ -0,0 +1,94 @@
+@extends('layouts.layout')
+
+@section('title') Attributes @endsection
+
+@section('scripts')
+    <script>
+        $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')})
+    </script>
+@endsection
+
+@section('content')
+    <div class="container-fluid p-3">
+        <div class="row pb-3">
+            <div class="col-md-12">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Add Attribute
+                </button>
+            </div>
+        </div>
+        @if ($message = Session::get('success'))
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                </div>
+            </div>
+        </div>
+        @endif
+        @if (count($attributes)!=0)
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Label</th>
+                            <th scope="col">EnityType</th>
+                            <th scope="col">Method</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($attributes as $attribute)
+                        <tr>
+                            <td>{{$attribute->id}}</td>
+                            <td>{{$attribute->label}}</td>
+                            <td>{{$attribute->entityType->label}}</td>
+                            <td>
+                                <form action="{{ route('attribute.destroy',$attribute) }}" method="POST">
+    
+                                    @csrf
+                                    @method('DELETE')
+    
+                                    <button type="button" class="btn btn-warning">Modify</button>
+                    
+                                    <button type="submit" onclick="return confirm('Delete  entity type?')" class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @else
+            <h1 class="display-4 d-flex justify-content-center">There aren't any attributes in the database.</h1>
+        @endif
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add new entity type</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('entity-type.store') }}" method="POST">
+                        @csrf
+                        
+                        <div class="form-group">
+                            <strong>Entity type label:</strong>
+                            <input type="text" name="label" class="form-control mt-3" placeholder="Label">
+                        </div>
+                        <button type="submit" class="btn btn-success">Add</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+@endsection
