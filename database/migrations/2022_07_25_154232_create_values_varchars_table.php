4d3a9dcba4fdefe3022598de29df1d3b9edd6836
csik-patrik <csik.patrik455@gmail.com>
2022-07-25 17:50:06 +0200

    Implement values migration

---
+++ database/migrations/2022_07_25_154232_create_values_varchars_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('values_varchars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('entity_id');
+            $table->unsignedBigInteger('type_id');
+            $table->unsignedBigInteger('attribute_id');
+            $table->string('value');
+
+            $table->foreign('type_id')->references('id')->on('entity_types');
+            $table->foreign('attribute_id')->references('id')->on('attributes');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('values_varchars');
+    }
+};
