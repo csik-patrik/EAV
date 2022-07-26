3b4179201abb847612b4b90cbd834025a030cd3b
csik-patrik <csik.patrik455@gmail.com>
2022-07-26 18:21:14 +0200

    Add entity link for nav

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -6,6 +6,7 @@
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-item nav-link" href="{{url('entity-type')}}">Entity types</a>
+        <a class="nav-item nav-link" href="{{url('entity')}}">Entities</a>
         <a class="nav-item nav-link" href="{{url('attribute')}}">Attributes</a>
       </div>
     </div>
