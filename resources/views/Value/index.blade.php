d456583ff73f3300095aad4a9d2cf94fba3aac83
csik-patrik <csik.patrik455@gmail.com>
2022-07-28 15:49:27 +0200

    Change message popup

--- resources/views/Value/index.blade.php
+++ resources/views/Value/index.blade.php
@@ -20,9 +20,12 @@
         </div>
         @if ($message = Session::get('success'))
         <div class="row">
-            <div class="col-md-12">
-                <div class="alert alert-success">
+            <div class="col-md-6">
+                <div class="alert alert-success alert-dismissible fade show" role="alert" >
                     <p>{{ $message }}</p>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
             </div>
         </div>
