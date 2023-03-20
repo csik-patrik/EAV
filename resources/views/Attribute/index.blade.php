b3a6baa6cdc987ff9ad60a8d92f7ee46d631fdec
csik-patrik <csi.patrik455@gmail.com>
2023-03-20 13:17:48 +0100

    Refactor modal

--- resources/views/Attribute/index.blade.php
+++ resources/views/Attribute/index.blade.php
@@ -68,37 +68,7 @@
         @endif
     </div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="insertAttributeModal" tabindex="-1" role="dialog" aria-labelledby="insertAttributeModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                <h5 class="modal-title" id="insertAttributeModalLabel">Add attribute</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                </div>
-                <div class="modal-body">
-                    <form action="{{ route('attribute.store') }}" method="POST">
-                        @csrf
-                        
-                        <div class="form-group">
-                            <strong>Attribute label:</strong>
-                            <input type="text" name="attribute_label" class="form-control mt-3" placeholder="Label">
-
-                            <strong class="mt-3 mb-3">Attribute type:</strong>
-                            <select name="type_id" class="form-control mt-3">
-                                @foreach ($entityTypes as $entityType)
-                                    <option value="{{$entityType->id}}">{{$entityType->entity_type_label}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <button type="submit" class="btn btn-success">Add</button>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- Create attribute modal -->
+    @include('Modals.Attribute.create');
     
 @endsection
