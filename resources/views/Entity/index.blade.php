c8a0e09b09f96e037901dbd087ce366f3846a513
csik-patrik <csi.patrik455@gmail.com>
2023-03-20 13:06:47 +0100

    Add validation message

--- resources/views/Entity/index.blade.php
+++ resources/views/Entity/index.blade.php
@@ -18,6 +18,18 @@
                 </button>
             </div>
         </div>
+        @error('type_id')
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-danger alert-dismissible fade show" role="alert" >
+                    <p>{{ $message }}</p>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+        @enderror
         @if ($message = Session::get('success'))
         <div class="row">
             <div class="col-md-12">
