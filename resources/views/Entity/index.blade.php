151689fc3b53313dab6c6f9be14149017e92d633
csik-patrik <csi.patrik455@gmail.com>
2023-03-20 13:09:12 +0100

    Update message alerts

--- resources/views/Entity/index.blade.php
+++ resources/views/Entity/index.blade.php
@@ -31,13 +31,16 @@
         </div>
         @enderror
         @if ($message = Session::get('success'))
-        <div class="row">
-            <div class="col-md-12">
-                <div class="alert alert-success">
-                    <p>{{ $message }}</p>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="alert alert-success alert-dismissible fade show" role="alert" >
+                        <p>{{ $message }}</p>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
         @endif
         @if (count($entities)!=0)
             <div class="table-responsive">
