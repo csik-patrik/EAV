59f6827cb37cb9122a83496a11f193ff039fea45
csik-patrik <csi.patrik455@gmail.com>
2023-03-21 11:08:46 +0100

    Fix array problem

--- app/Http/Livewire/ReportGenerator.php
+++ app/Http/Livewire/ReportGenerator.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Attribute;
+use App\Models\Entity;
 use App\Models\EntityType;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
@@ -15,36 +17,39 @@ class ReportGenerator extends Component
     public $attributes;
 
     public $selectedEntityTypeId = 1;
-
     public $selectedAttributes = [];
+
+    public $reportGenerated = false;
 
     public function change()
     {
         try {
-            $this->entities = DB::table('entities')->where('type_id', $this->selectedEntityTypeId)->get();
 
-            $this->attributes = DB::table('attributes')->where('type_id', $this->selectedEntityTypeId)->get();
+            $this->selectedAttributes = [];
+
+            $this->entities = Entity::where('type_id', $this->selectedEntityTypeId)->get();
+
+            $this->attributes = Attribute::where('type_id', $this->selectedEntityTypeId)->get();
 
         } catch(\Illuminate\Database\QueryException $exception) {
+
         }
     }
 
-    public function submit()
-    {
-        return $this;
+    public function generate(){
+        $this->reportGenerated = true;
     }
 
     public function mount()
     {
-        $this->entities = DB::table('entities')->where('type_id', $this->selectedEntityTypeId)->get();
-
-        $this->attributes = DB::table('attributes')->where('type_id', $this->selectedEntityTypeId)->get();
-
         $this->entityTypes = EntityType::get();
     }
 
     public function render()
     {
-        return view('livewire.report-generator');
+        if(!$this->reportGenerated){
+            return view('livewire.report-generator');
+        }
+        return view('livewire.report-generated');
     }
 }
--- resources/views/livewire/report-generator.blade.php
+++ resources/views/livewire/report-generator.blade.php
@@ -1,4 +1,4 @@
-<div class="container p-3 wire:ignore.self">
+<div class="container p-3">
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12">
                 <strong class="mt-3 mb-3">Entity type:</strong>
@@ -24,27 +24,23 @@
                 @if(is_null($attributes))
                     <strong class="mt-3 mb-3">Select entity type first!</strong>
                 @else
-                    <div class="overflow-auto">
-                        <input checked class="form-check-input"
-                               type="checkbox"
-                               value="1"
-                               wire:model="selectedAttributes">
+                    <div class="overflow-auto" >
                         @foreach($attributes as $attribute)
                             <div class="form-check form-switch">
                                 <input checked class="form-check-input"
                                        type="checkbox"
+                                       id="{{$attribute->attribute_label}}"
                                        value="{{$attribute->attribute_label}}"
                                        wire:model="selectedAttributes">
                                 <label class="form-check-label"
-                                       for="{{$attribute->id}}">{{$attribute->attribute_label}}</label>
+                                       for="{{$attribute->attribute_label}}">{{$attribute->attribute_label}}</label>
                             </div>
                         @endforeach
                     </div>
                 @endif
             </div>
         </div>
-        {{var_export($selectedAttributes)}}
         <div class="row">
-            <button type="submit" class="btn btn-success">Generate report</button>
+            <button type="submit" class="btn btn-success" wire:click="generate">Generate report</button>
         </div>
 </div>
