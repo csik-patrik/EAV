a720125e67acacf741faf996e0d8605a65e13d3a
csik-patrik <csi.patrik455@gmail.com>
2023-03-27 19:02:27 +0200

    Add: Handling empty values case

--- resources/views/livewire/report.blade.php
+++ resources/views/livewire/report.blade.php
@@ -1,45 +1,49 @@
-<table class="table">
-    <thead class="thead-dark">
-    <tr>
-        <th scope="col">ID</th>
-        <!-- List of selected attributes as table headers -->
-        @foreach($attributes as $attribute)
-            <th scope="col">{{$attribute->attribute_label}}</th>
-        @endforeach
-    </tr>
-    </thead>
-    <tbody>
-    @php
-        $tempId = $values[0]->entity_id;
-        $actColIndex = -1;
-    @endphp
-    @for($i = 0; $i < count($values); $i++)
-        @if($i == 0)
-            <tr>
-                <td>{{$values[$i]->entity_id}}</td>
-        @endif
-        @if($values[$i]->entity_id != $tempId && $i != 0)
-            </tr>
-            @php
-                $actColIndex = -1;
-            @endphp
-            <tr>
-                <td>{{$values[$i]->entity_id}}</td>
-            @php
-                $tempId = $values[$i]->entity_id;
-            @endphp
-        @endif
+@if(count($values)==0)
+    <h1>There are 0 records for these parameters!</h1>
+@else
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">ID</th>
+            <!-- List of selected attributes as table headers -->
+            @foreach($attributes as $attribute)
+                <th scope="col">{{$attribute->attribute_label}}</th>
+            @endforeach
+        </tr>
+        </thead>
+        <tbody>
         @php
-            $actColIndex++;
+            $tempId = $values[0]->entity_id;
+            $actColIndex = -1;
         @endphp
-            <!-- Moving to the correct column -->
-            @while($attributes->has($actColIndex) &&  $values[$i]->attribute_id != $attributes[$actColIndex]->id)
+        @for($i = 0; $i < count($values); $i++)
+            @if($i == 0)
+                <tr>
+                    <td>{{$values[$i]->entity_id}}</td>
+                    @endif
+                    @if($values[$i]->entity_id != $tempId && $i != 0)
+                </tr>
                 @php
-                    $actColIndex++;
+                    $actColIndex = -1;
                 @endphp
-                <td></td>
-            @endwhile
-            <td>{{$values[$i]->value}}</td>
-    @endfor
-    </tbody>
-</table>
+                <tr>
+                    <td>{{$values[$i]->entity_id}}</td>
+                    @php
+                        $tempId = $values[$i]->entity_id;
+                    @endphp
+                    @endif
+                    @php
+                        $actColIndex++;
+                    @endphp
+                            <!-- Moving to the correct column -->
+                    @while($attributes->has($actColIndex) &&  $values[$i]->attribute_id != $attributes[$actColIndex]->id)
+                        @php
+                            $actColIndex++;
+                        @endphp
+                        <td></td>
+                    @endwhile
+                    <td>{{$values[$i]->value}}</td>
+                @endfor
+        </tbody>
+    </table>
+@endif
